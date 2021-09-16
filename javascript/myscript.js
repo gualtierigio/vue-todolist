16d3a397f74dac226df5791b8d71f193773251ae
gualtierigio <gualtierigio@gmail.com>
2021-09-16 15:13:38 +0200

    primo step completato

---
+++ javascript/myscript.js
@@ -0,0 +1,15 @@
+var groot = new Vue({
+    el: '#groot',
+    data: {
+      listItems: [
+          'patate',
+          'cipolle',
+          'zucchine',
+          'melanzane',
+          'happy-hippo',
+          'parmigiano',
+          'latte'
+      ]
+    },
+    methods: {}
+  });
