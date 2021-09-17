64a1f7babfa0240beeba508befd1dac86a4913d8
gualtierigio <gualtierigio@gmail.com>
2021-09-17 11:29:15 +0200

    aggiunto funzione line-through

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -2,24 +2,46 @@ var groot = new Vue({
     el: '#groot',
     data: {
       listItems: [
-          'patate',
-          'cipolle',
-          'zucchine',
-          'melanzane',
-          'happy-hippo',
-          'parmigiano',
-          'latte'
-      ],
+        { content: 'patate',
+          done: false
+        },
+        { content: 'cipolle',
+          done: false
+        },
+        { content: 'zucchine',
+          done: false
+        },
+        { content: 'birre',
+          done: false
+        },
+        { content: 'pane',
+          done: false
+        },
+        { content: 'mortadella',
+          done: false
+        },
+        { content: 'salame',
+          done: false
+        },],
       newItem: '',
     },
     methods: {
+
         checked: function(index){
             this.listItems.splice(index, 1)
             
         },
+
         importNewItem: function(){
-            this.listItems.push(this.newItem)
-            this.newItem = '' 
+
+            if (this.newItem.trim().length > 0) {
+                this.listItems.push(this.newItem)
+                this.newItem = '' 
+            }
+        },
+
+        sbarraElemento(index) {
+            this.listItems[index].done = ! this.listItems[index].done;
         }
     }
   });
